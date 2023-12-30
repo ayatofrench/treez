d8fec177f9401da464a3a6081d15341f31377005
Ayato French <aj@pop-os.localdomain>
2023-12-29 23:21:48 -0600

    Add match predicate

--- treez.zig
+++ treez.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Regex = @import("regex").Regex;
 
 // TREE_SITTER_LANGUAGE_VERSION 14
 // TREE_SITTER_MIN_COMPATIBLE_LANGUAGE_VERSION 13
@@ -831,12 +832,27 @@ pub const Query = opaque {
 pub const CursorWithValidation = struct {
     // We only support #eq?
 
+    const Predicates = enum {
+        @"eq?",
+        @"match?",
+    };
+
     pub const EqualPredicate = struct {
         a: []const u8,
         b: union(enum) { string: []const u8, capture: []const u8 },
     };
 
-    pub const PredicateList = std.ArrayListUnmanaged(EqualPredicate);
+    pub const MatchPredicate = struct {
+        a: []const u8,
+        regex: *Regex,
+    };
+
+    pub const TextPredicate = union(enum) {
+        equal_predicate: EqualPredicate,
+        match_predicate: MatchPredicate,
+    };
+
+    pub const PredicateList = std.ArrayListUnmanaged(TextPredicate);
     pub const PredicateMap = std.AutoHashMapUnmanaged(u32, packed struct { index: u32, len: u32 });
 
     pub const CaptureIdNameMap = std.StringHashMapUnmanaged(u32);
@@ -860,27 +876,53 @@ pub const CursorWithValidation = struct {
             var index: usize = 0;
             var predicate_len: u32 = 0;
             while (index < preds.len) {
-                if (preds[index].type != .string) @panic("Unexpected predicate value");
-                if (!std.mem.eql(u8, query.getStringValueForId(@as(u32, @intCast(preds[index].value_id))), "eq?")) @panic("Only the 'eq?' predicate is supported by treez at the moment.");
-                if (preds[index + 1].type != .capture) @panic("Unexpected predicate value");
+                const pred = query.getStringValueForId(@as(u32, @intCast(preds[index].value_id)));
 
-                switch (preds[index + 2].type) {
-                    .string => {
-                        try predicates.append(allocator, .{
-                            .a = query.getCaptureNameForId(@as(u32, @intCast(preds[index + 1].value_id))),
-                            .b = .{ .string = query.getStringValueForId(@as(u32, @intCast(preds[index + 2].value_id))) },
+                switch (std.meta.stringToEnum(Predicates, pred).?) {
+                    .@"eq?" => {
+                        if (preds[index].type != .string) @panic("Unexpected predicate value");
+                        if (preds[index + 1].type != .capture) @panic("Unexpected predicate value");
+                        // if (!std.mem.eql(u8, query.getStringValueForId(@as(u32, @intCast(preds[index].value_id))), "eq?")) {
+                        //     // std.debug.print("predicate: {s}", .{query.getStringValueForId(@as(u32, @intCast(preds[index].value_id)))});
+                        //     @panic("Only the 'eq?' predicate is supported by treez at the moment.");
+                        // }
+
+                        switch (preds[index + 2].type) {
+                            .string => {
+                                try predicates.append(allocator, TextPredicate{
+                                    .equal_predicate = .{
+                                        .a = query.getCaptureNameForId(@as(u32, @intCast(preds[index + 1].value_id))),
+                                        .b = .{ .string = query.getStringValueForId(@as(u32, @intCast(preds[index + 2].value_id))) },
+                                    },
+                                });
+                            },
+                            .capture => {
+                                try predicates.append(allocator, TextPredicate{
+                                    .equal_predicate = .{
+                                        .a = query.getCaptureNameForId(@as(u32, @intCast(preds[index + 1].value_id))),
+                                        .b = .{ .capture = query.getCaptureNameForId(@as(u32, @intCast(preds[index + 2].value_id))) },
+                                    },
+                                });
+                            },
+                            else => @panic("Unexpected predicate value"),
+                        }
+
+                        if (preds[index + 3].type != .done) @panic("Unexpected predicate value");
+                    },
+                    .@"match?" => {
+                        const regex_string = query.getStringValueForId(@as(u32, @intCast(preds[index + 2].value_id)));
+                        const re = try allocator.create(Regex);
+                        re.* = Regex.compile(allocator, regex_string) catch @panic("");
+
+                        try predicates.append(allocator, TextPredicate{
+                            .match_predicate = .{
+                                .a = query.getCaptureNameForId(@as(u32, @intCast(preds[index + 1].value_id))),
+                                .regex = re,
+                            },
                         });
                     },
-                    .capture => {
-                        try predicates.append(allocator, .{
-                            .a = query.getCaptureNameForId(@as(u32, @intCast(preds[index + 1].value_id))),
-                            .b = .{ .capture = query.getCaptureNameForId(@as(u32, @intCast(preds[index + 2].value_id))) },
-                        });
-                    },
-                    else => @panic("Unexpected predicate value"),
+                    // else => @panic("Only the 'eq?' and 'match?' predicates are supported by treez at the moment."),
                 }
-
-                if (preds[index + 3].type != .done) @panic("Unexpected predicate value");
 
                 // TODO: This is here as we'll need to tweak these to support future predicates
                 predicate_len += 1;
@@ -914,31 +956,47 @@ pub const CursorWithValidation = struct {
 
     pub fn isValid(validator: CursorWithValidation, source: []const u8, match: Query.Match) bool {
         if (validator.predicate_map.get(match.pattern_index)) |pred_loc| {
-            const predicates: []const EqualPredicate = validator.predicates.items[pred_loc.index .. pred_loc.index + pred_loc.len];
+            const predicates: []const TextPredicate = validator.predicates.items[pred_loc.index .. pred_loc.index + pred_loc.len];
             for (predicates) |pred| {
-                const a = validator.capture_name_to_id.get(pred.a).?;
-                const b_capture = switch (pred.b) {
-                    .string => null,
-                    .capture => |c| validator.capture_name_to_id.get(c).?,
-                };
+                switch (pred) {
+                    .equal_predicate => |val| {
+                        const a = validator.capture_name_to_id.get(val.a).?;
+                        const b_capture = switch (val.b) {
+                            .string => null,
+                            .capture => |c| validator.capture_name_to_id.get(c).?,
+                        };
 
-                var a_value: ?[]const u8 = null;
-                var b_value: ?[]const u8 = switch (pred.b) {
-                    .string => |v| v,
-                    .capture => null,
-                };
+                        var a_value: ?[]const u8 = null;
+                        var b_value: ?[]const u8 = switch (val.b) {
+                            .string => |v| v,
+                            .capture => null,
+                        };
 
-                for (match.captures()) |cap| {
-                    if (cap.id == a) a_value = source[cap.node.getStartByte()..cap.node.getEndByte()];
-                    if (b_capture != null and cap.id == b_capture.?) b_value = source[cap.node.getStartByte()..cap.node.getEndByte()];
+                        for (match.captures()) |cap| {
+                            if (cap.id == a) a_value = source[cap.node.getStartByte()..cap.node.getEndByte()];
+                            if (b_capture != null and cap.id == b_capture.?) b_value = source[cap.node.getStartByte()..cap.node.getEndByte()];
+                        }
+
+                        const av = a_value orelse @panic("Impossible!");
+                        const bv = b_value orelse @panic("Impossible!");
+
+                        std.log.info("{s} {s}", .{ av, bv });
+
+                        if (!std.mem.eql(u8, av, bv)) return false;
+                    },
+                    .match_predicate => |val| {
+                        const a = validator.capture_name_to_id.get(val.a).?;
+                        var a_value: ?[]const u8 = null;
+
+                        for (match.captures()) |cap| {
+                            if (cap.id == a) a_value = source[cap.node.getStartByte()..cap.node.getEndByte()];
+                        }
+
+                        const res = val.regex.match(a_value.?) catch return false;
+
+                        if (!res) return false;
+                    },
                 }
-
-                const av = a_value orelse @panic("Impossible!");
-                const bv = b_value orelse @panic("Impossible!");
-
-                std.log.info("{s} {s}", .{ av, bv });
-
-                return std.mem.eql(u8, av, bv);
             }
         }
 
